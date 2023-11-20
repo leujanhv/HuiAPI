e157eb64fe46a391391e89096f5fdae296d2d017
huihui <liujianhuingp@163.com>
2023-11-20 15:45:27 +0800

    add README

---
+++ config/routes.ts
@@ -0,0 +1,17 @@
+﻿export default [
+  { name:'一',path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  { name:'二',path: '/welcome', icon: 'smile', component: './Welcome' },
+  {
+    name:'三',
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {name:'1', path: '/admin', redirect: '/admin/sub-page' },
+      {name:'2', path: '/admin/sub-page', component: './Admin' },
+    ],
+  },
+  { icon: 'table', path: '/list', component: './TableList' },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,25 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+const Footer: React.FC = () => {
+  const defaultMessage = 'Power By AntDesign Pro';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: <p>HuiAPI  <GithubOutlined /></p>,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
